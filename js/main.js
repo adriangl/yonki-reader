bff87cced9a7b5e0a3f48448383eda7d2bbf14b3
Adrian Garcia <agarcia@ipsvial.es>
2013-09-10 10:20:27 +0200

    Simple functionality working.

---
+++ js/main.js
@@ -0,0 +1,61 @@
+var RSS_FEED_URL = "http://yonkiblog.com/feed";
+
+var entries = null;
+
+function parseFeed(feedUrl){
+	// Use Google AJAX API to read the feed and return the parsed
+	// data as JSON
+	$.ajax({
+		url      : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&num=10&q=' + encodeURIComponent(feedUrl),
+		dataType : 'json',
+		success  : function (data) {
+			if (data.responseData.feed && data.responseData.feed.entries) {
+				entries = data.responseData.feed.entries;
+				loadPostsData();
+			}
+		},
+		error : function(){
+			console.log("Error!!");
+		}
+	});
+}
+
+function loadPostsData(){
+	var postsContainer = $("#posts-container");
+	$.each(entries, function(entryIdx){
+		var entry = entries[entryIdx];
+		var post = $("<div class=\"post\""+entryIdx+"></div>");
+		// Post title
+		var postTitle = $("<div class=\"post-title\"></div>");
+		$(postTitle).append("<h3>"+entry.title+"</h3>");
+		
+		// Post content
+		var postContent = $("<div class=\"post-content\"></div>");
+		$(postContent).hide();
+
+		$(postContent).append(entry.content);
+
+		$(post).append(postTitle);
+		$(post).append(postContent);
+
+		$(post).click(function(){
+			var content = $(post).find(".post-content");
+			if ($(content).hasClass("enabled")){
+				$(content).hide('slow');
+				$(content).toggleClass("enabled");
+			}
+			else{
+				$(document).removeClass("enabled");
+				$('.post-content').hide('slow');
+				$(content).toggleClass("enabled");
+				$(content).show("slow");
+			}
+		})
+
+		$(postsContainer).append(post);
+	});
+}
+
+function main(){
+	parseFeed(RSS_FEED_URL);
+}
