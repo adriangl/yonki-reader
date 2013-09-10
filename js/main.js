52f365ce1fe7613606fa0afab432ad7de245559c
Adrian Garcia <agarcia@ipsvial.es>
2013-09-10 14:59:32 +0200

    Added Font Awesome resources to layout.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-var RSS_FEED_URL = "http://yonkiblog.com/feed";
+var BASE_URL = "http://yonkiblog.com";
+var RSS_FEED_URL = BASE_URL+"/feed";
+var TAG_URL = BASE_URL+"/tags";
 
 var entries = null;
 
@@ -25,22 +27,39 @@ function loadPostsData(){
 	$.each(entries, function(entryIdx){
 		var entry = entries[entryIdx];
 		var post = $("<div class=\"well post-"+entryIdx+"\"></div>");
+		
 		// Post title
 		var postTitle = $("<div class=\"post-title\"></div>");
-		$(postTitle).append("<h4>"+entry.title+"</h4>");
-		$(postTitle).append("<em><small>"+parseDate(entry.publishedDate)+"</small></em>");
-		
+		$(postTitle).append("<h4 class=\"inline-block\">"+entry.title+"</h4>");
+		// Add icon
+		$(postTitle).append("<i class=\"icon-chevron-right icon-2x pull-right\"></i>");
+
 		// Post content
 		var postContent = $("<div class=\"post-content\"></div>");
-		$(postContent).hide();
-
 		$(postContent).append(entry.content);
+		$(postContent).append("<input type=\"button\" onclick=\"window.open("+entry.link+")\" value=\"Ver en la web\">");
+
+		$(postContent).hide();
 
 		$(post).append(postTitle);
 		$(post).append(postContent);
 
+		// Add tags and date
+		$(post).append("<em><small>"+parseDate(entry.publishedDate)+"</small></em>");
+		var categoriesDiv = $("<div class=\"categories\"></div>");
+		for (catIdx in entry.categories){
+			var category = entry.categories[catIdx];
+			$(categoriesDiv).append("<a target=\"_blank\" href=\""+TAG_URL+"/"+category+"\"><span class=\"label label-info\">"+category+"</span></a>");
+		}
+		$(post).append(categoriesDiv);
+
 		$(post).click(function(){
 			var content = $(post).find(".post-content");
+			var downIcon = $(document).find(".icon-chevron-down");
+			if (downIcon != null){				
+				$(downIcon).removeClass("icon-chevron-down").addClass("icon-chevron-right");
+			}
+
 			if ($(content).hasClass("enabled")){
 				$(content).hide('slow');
 				$(content).toggleClass("enabled");
@@ -49,6 +68,8 @@ function loadPostsData(){
 				$(document).removeClass("enabled");
 				$('.post-content').hide('slow');
 				$(content).toggleClass("enabled");
+				var icon = $(post).find(".icon-chevron-right");
+				$(icon).removeClass("icon-chevron-right").addClass("icon-chevron-down");
 				$(content).show("slow");
 			}
 		})
