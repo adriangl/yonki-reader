35cccb468b1c0a3ac9ae246a00ad84ceeaa112f5
Adrian Garcia <adriangarcia.lopez@gmail.com>
2013-09-10 23:12:49 +0200

    Changes in stylesheets and behaviour.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var BASE_URL = "http://yonkiblog.com";
 var RSS_FEED_URL = BASE_URL+"/feed";
-var TAG_URL = BASE_URL+"/tags";
+var TAG_URL = BASE_URL+"/tag";
+var CATEGORY_URL = BASE_URL+"/category";
 
 var entries = null;
 
@@ -8,7 +9,7 @@ function parseFeed(feedUrl){
 	// Use Google AJAX API to read the feed and return the parsed
 	// data as JSON
 	$.ajax({
-		url      : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&num=50&q=' + encodeURIComponent(feedUrl),
+		url      : 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&num=50&q=' + encodeURIComponent(feedUrl),
 		dataType : 'json',
 		success  : function (data) {
 			if (data.responseData.feed && data.responseData.feed.entries) {
@@ -26,7 +27,7 @@ function loadPostsData(){
 	var postsContainer = $("#posts-container");
 	$.each(entries, function(entryIdx){
 		var entry = entries[entryIdx];
-		var post = $("<div class=\"well post-"+entryIdx+"\"></div>");
+		var post = $("<div class=\"well\" id=\"post-"+entryIdx+"\"></div>");
 		
 		// Post title
 		var postTitle = $("<div class=\"post-title\"></div>");
@@ -37,42 +38,57 @@ function loadPostsData(){
 		// Post content
 		var postContent = $("<div class=\"post-content\"></div>");
 		$(postContent).append(entry.content);
-		$(postContent).append("<input type=\"button\" onclick=\"window.open("+entry.link+")\" value=\"Ver en la web\">");
+		$(postContent).append("<input type=\"button\" onclick=\"window.open('"+entry.link+"');\" value=\"Ver en la web\">");
 
 		$(postContent).hide();
 
 		$(post).append(postTitle);
 		$(post).append(postContent);
 
-		// Add tags and date
+		// Add CATEGORY, tags and date
 		$(post).append("<em><small>"+parseDate(entry.publishedDate)+"</small></em>");
 		var categoriesDiv = $("<div class=\"categories\"></div>");
 		for (catIdx in entry.categories){
 			var category = entry.categories[catIdx];
-			$(categoriesDiv).append("<a target=\"_blank\" href=\""+TAG_URL+"/"+category+"\"><span class=\"label label-info\">"+category+"</span></a>");
+			var url = "";
+			var labelStyle = ""
+			if (catIdx == 0){
+				labelStyle = "label-info"
+				$(categoriesDiv).append("<span class=\"label "+labelStyle+"\">"+category+"</span>");
+			}
+			else{
+				url = TAG_URL+"/"+category;
+				$(categoriesDiv).append("<a target=\"_blank\" href=\""+url+"\"><span class=\"label\">"+category+"</span></a>");
+			}
+			
 		}
 		$(post).append(categoriesDiv);
 
-		$(post).click(function(){
+
+		var clickHandler = function(){
 			var content = $(post).find(".post-content");
 			var downIcon = $(document).find(".icon-chevron-down");
 			if (downIcon != null){				
 				$(downIcon).removeClass("icon-chevron-down").addClass("icon-chevron-right");
 			}
-
 			if ($(content).hasClass("enabled")){
 				$(content).hide('slow');
-				$(content).toggleClass("enabled");
+				$(content).removeClass("enabled");
 			}
 			else{
 				$(document).removeClass("enabled");
 				$('.post-content').hide('slow');
-				$(content).toggleClass("enabled");
+				$(content).addClass("enabled");
 				var icon = $(post).find(".icon-chevron-right");
 				$(icon).removeClass("icon-chevron-right").addClass("icon-chevron-down");
-				$(content).show("slow");
+				$(content).show("slow", function(){
+					location.href = "#";
+					location.href = "#post-"+entryIdx;
+				});
 			}
-		})
+		};
+
+		$(postTitle).click(clickHandler);
 
 		$(postsContainer).append(post);
 	});
